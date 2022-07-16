94cd087797be2bfb8a248b1b709e231f27189ee5
Gaku Sato <ggg514@icloud.com>
2022-07-17 00:41:21 +0900

    追従処理を呼ぶメソッドの変更

--- Assets/_MyAssets/Script/CameraController.cs
+++ Assets/_MyAssets/Script/CameraController.cs
@@ -16,7 +16,8 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+
+    void LateUpdate()
     {
         currentZ = Mathf.SmoothDamp(transform.position.z, targetPosition.position.z + offsetZ, ref _currentVelocity, _smoothTime);
         transform.position = new Vector3(transform.position.x, transform.position.y, currentZ);
